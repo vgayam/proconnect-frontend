fd3615068ac54b94152fc68d60376c1c90f59ac4
v0g01dq <v.gayam@walmart.com>
2026-02-21 20:24:38 +0530

    fix: remove duplicate List Your Services from nav

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,7 +14,6 @@ import { cn } from "@/lib/utils";
 
 const NAV_LINKS = [
   { href: "/professionals", label: "Find Professionals" },
-  { href: "/list-service", label: "List Your Services" },
 ];
 
 /**
